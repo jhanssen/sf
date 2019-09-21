19611b0b08f936c23ba296c7aaa2940916cd93f8
Jan Erik Hanssen <jhanssen@gmail.com>
2019-09-21 02:14:18 -0700

    I need to improve the pad performance

--- format/print.cpp
+++ format/print.cpp
@@ -997,7 +997,7 @@ int main(int, char**)
     for (int i = 0; i < Iter; ++i) {
         //snprint(buffer, sizeof(buffer), "hello2 %f\n", 12234.15281);
         //snprint(buffer, sizeof(buffer), "hello2 %20s\n", "hipphipp");
-        snprint(buffer1, sizeof(buffer1), "hello2 %#x%s%u%n%p%n%s\n%n", 1234567, "jappja", 12345, &n1, &n1, &n2, "trall og trall", &fn1);
+        snprint(buffer1, sizeof(buffer1), "hello2 %#x%s%u%n%140p%n%s\n%n", 1234567, "jappja", 12345, &n1, &n1, &n2, "trall og trall", &fn1);
     }
 
     auto t2 = steady_clock::now();
@@ -1007,7 +1007,7 @@ int main(int, char**)
     for (int i = 0; i < Iter; ++i) {
         //snprintf(buffer, sizeof(buffer), "hello1 %f\n", 12234.15281);
         //snprintf(buffer, sizeof(buffer), "hello1 %20s\n", "hipphipp");
-        snprintf(buffer2, sizeof(buffer2), "hello2 %#x%s%u%n%p%n%s\n%n", 1234567, "jappja", 12345, &n1, &n1, &n2, "trall og trall", &fn2);
+        snprintf(buffer2, sizeof(buffer2), "hello2 %#x%s%u%n%140p%n%s\n%n", 1234567, "jappja", 12345, &n1, &n1, &n2, "trall og trall", &fn2);
     }
 
     auto t4 = steady_clock::now();
